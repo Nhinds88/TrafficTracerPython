21b645b34612cbc488c4a01cf706771c19e623ab
Nhinds88 <nhinds@csumb.edu>
2020-09-02 16:52:40 -0700

    people counter working

--- people_counter.py
+++ people_counter.py
@@ -7,20 +7,15 @@ Created on Tue Sep  1 11:34:28 2020
 
 import numpy
 import cv2
-#import Person
-#import time
 import tools
 
 try: 
     log = open('log.txt', 'w')
 except:
     print('Cannot open log file')
-    
-enterCount = 0
-exitCount = 0
 
-cameraFeed = tools.cameraSource(0)
-
+#cameraFeed = tools.cameraSource(0)
+cameraFeed = tools.cameraSource('TestVideo.avi')
 
 video = cv2.VideoCapture(cameraFeed)
 
@@ -29,10 +24,10 @@ width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
 frameArea = height * width
 areaThreshold = frameArea/250
 
-entry_line = tools.lineCrossingPlacement((2*(height/5)), 'y')
-exit_line = tools.lineCrossingPlacement((3*(height/5)), 'y')
-entry_line_Limit = tools.lineCrossingPlacement((1*(height/5)), 'y')
-exit_line_Limit = tools.lineCrossingPlacement((4*(height/5)), 'y')
+entry_line = tools.lineCrossingPlacement(int((2*(height/5))), 'y')
+exit_line = tools.lineCrossingPlacement(int(entry_line[0] + 100), 'y')
+entry_line_Limit = tools.lineCrossingPlacement(int((1*(height/5))), 'y')
+exit_line_Limit = tools.lineCrossingPlacement(int((4*(height/5))), 'y')
                                         
 point1 = [0,0]
 point2 = [0,0]
@@ -46,43 +41,51 @@ point8 = [0,0]
 if entry_line[1] == 'y':
     point3 = [0, entry_line[0]]
     point4 = [width, entry_line[0]]
+    print('Entry line Y')
 else:
     point3 = [0, entry_line[0]]
     point4 = [height, entry_line[0]]
+    print('Entry line X')
 
 if exit_line[1] == 'y':
     point1 = [0, exit_line[0]]
     point2 = [width, exit_line[0]]
+    print('Exit line Y')
 else:
     point1 = [0, exit_line[0]]
     point2 = [height, exit_line[0]]
+    print('Exit line X')
     
 if entry_line_Limit[1] == 'y':
     point5 = [0, entry_line_Limit[0]]
     point6 = [width, entry_line_Limit[0]]
+    print('Entry line Limit Y')
 else:
     point5 = [0, entry_line_Limit[0]]
     point6 = [height, entry_line_Limit[0]]
+    print('Entry line Limit X')
     
 if exit_line_Limit[1] == 'y':
     point7 = [0, exit_line_Limit[0]]
     point8 = [width, exit_line_Limit[0]]
+    print('Exit line Limit Y')
 else:
     point7 = [0, exit_line_Limit[0]]
     point8 = [height, exit_line_Limit[0]]
+    print('Exit line Limit X')
     
-# =============================================================================
+# =============================================================================x
 line1_points = numpy.array([point1,point2], numpy.int32)
 line1_points = line1_points.reshape((-1,1,2))
 # =============================================================================
 line2_points = numpy.array([point3,point4], numpy.int32)
-line2_points = line1_points.reshape((-1,1,2))
+line2_points = line2_points.reshape((-1,1,2))
 # =============================================================================
 line3_points = numpy.array([point5,point6], numpy.int32)
-line3_points = line1_points.reshape((-1,1,2))
+line3_points = line3_points.reshape((-1,1,2))
 # =============================================================================
 line4_points = numpy.array([point7,point8], numpy.int32)
-line4_points = line1_points.reshape((-1,1,2))
+line4_points = line4_points.reshape((-1,1,2))
 # =============================================================================
 
 persons = []
@@ -94,7 +97,13 @@ while video.isOpened():
     for i in persons:
         i.age_one()
         
-    tools.process_video(log, ret, frame, enterCount, exitCount, areaThreshold, entry_line_Limit, entry_line, exit_line_Limit, exit_line, line1_points, line2_points, line3_points, line4_points, persons)
+    if ret == True:
+        tools.process_video(log, ret, frame, areaThreshold, 
+                            entry_line_Limit, entry_line, 
+                            exit_line_Limit, exit_line, 
+                            line1_points, line2_points, 
+                            line3_points, line4_points, 
+                            persons)
     
     if cv2.waitKey(5) == ord('x'):
             break
--- tools.py
+++ tools.py
@@ -13,8 +13,11 @@ import Person
 subtractor = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
 
 Op = numpy.ones((3,3),numpy.uint8)
-Op2 = numpy.ones((5,5),numpy.uint8)
 Cl = numpy.ones((11,11),numpy.uint8)
+
+pid = 0
+entry = 0
+exited = 0
 
 def cameraSource(source):
     return source
@@ -22,10 +25,13 @@ def cameraSource(source):
 def lineCrossingPlacement(num, XorY):
     return num, XorY
 
-def process_video(log, ret, frame, entry, exited, areaThreshold, entry_limit, entry_line, exit_limit, exit_line, line1, line2, line3, line4, persons):
+def process_video(log, ret, frame, areaThreshold, entry_limit, entry_line, exit_limit, exit_line, line1, line2, line3, line4, persons):
     font = cv2.FONT_ITALIC
     max_p_age = 5
-    pid = 1
+    
+    global pid
+    global entry
+    global exited
     
     mask1 = subtractor.apply(frame)
     mask2 = subtractor.apply(frame)
@@ -33,18 +39,17 @@ def process_video(log, ret, frame, entry, exited, areaThreshold, entry_limit, en
     try:
         ret, imbin1 = cv2.threshold(mask1, 200, 255, cv2.THRESH_BINARY)
         ret, imbin2 = cv2.threshold(mask2, 200, 255, cv2.THRESH_BINARY)
-        
+         
         #Opening
         maskA = cv2.morphologyEx(imbin1, cv2.MORPH_OPEN, Op)
         maskB = cv2.morphologyEx(imbin2, cv2.MORPH_OPEN, Op)
-        
+         
         #Closing
         maskA = cv2.morphologyEx(maskA, cv2.MORPH_CLOSE, Cl)
         maskB = cv2.morphologyEx(maskB, cv2.MORPH_CLOSE, Cl)
     except :
-        print('EOF')
-        print('UP:', entry)
-        print('DOWN:', exited)
+        print('Enter: ', entry)
+        print('Exited: ', exited)
         
     
     contours, hierarchy = cv2.findContours(maskB, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -68,19 +73,21 @@ def process_video(log, ret, frame, entry, exited, areaThreshold, entry_limit, en
                         i.updateCoords(cx, cy)
                         if i.going_UP(exit_line[0], entry_line[0]) == True:
                             entry += 1
-                            print( "ID:",i.getId(),'crossed going up at',time.strftime("%c"))
-                            log.write("ID: "+str(i.getId())+' crossed going up at ' + time.strftime("%c") + '\n')
+                            print('Entered ' + str(entry))
+                            # print( "ID:",i.getId(),'crossed going up at',time.strftime("%c"))
+                            log.write("ID: "+str(i.getId())+' crossed going Entry at ' + time.strftime("%c") + '\n')
                         elif i.going_DOWN(exit_line[0], entry_line[0]) == True:
                             exited += 1
-                            print( "ID:",i.getId(),'crossed going down at',time.strftime("%c"))
-                            log.write("ID: " + str(i.getId()) + ' crossed going down at ' + time.strftime("%c") + '\n')
+                            print('Exited ' + str(exited))
+                            # print( "ID:",i.getId(),'crossed going down at',time.strftime("%c"))
+                            log.write("ID: " + str(i.getId()) + ' crossed going Exit at ' + time.strftime("%c") + '\n')
                         break
                     
                     if i.getState() == '1':
                         
-                        if i.getDir() == 'down' and i.getY() > exit_limit:
+                        if i.getDir() == 'exit' and i.getY() > exit_limit:
                             i.setDone()
-                        elif i.getDir() == 'up' and i.getY() < entry_limit:
+                        elif i.getDir() == 'entry' and i.getY() < entry_limit:
                             i.getDone()
                     if i.timedOut():
                         
@@ -92,17 +99,17 @@ def process_video(log, ret, frame, entry, exited, areaThreshold, entry_limit, en
                     p = Person.MyPerson(pid, cx, cy, max_p_age)
                     persons.append(p)
                     pid += 1
+                    
             cv2.circle(frame, (cx, cy), 5, (0,0,255), -1)
-            # img = cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
             
     for i in persons:
         cv2.putText(frame, str(i.getId()), (i.getX(), i.getY()), font, 0.3, i.getRGB(), 1, cv2.LINE_AA)
         
     enter_str = 'Entered: ' + str(entry)
-    exit_str = 'exited: ' + str(exited)
+    exit_str = 'Exited: ' + str(exited)
     
-    frame = cv2.polylines(frame, [line1], False, (255,0,0), thickness=3)
-    frame = cv2.polylines(frame, [line2], False, (0,0,255), thickness=3)
+    frame = cv2.polylines(frame, [line1], False, (255,0,0), thickness=2)
+    frame = cv2.polylines(frame, [line2], False, (0,0,255), thickness=2)
     frame = cv2.polylines(frame, [line3], False, (255,255,255), thickness=1)
     frame = cv2.polylines(frame, [line4], False, (255,255,255), thickness=1)
     
