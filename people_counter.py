d00571686d10b54a1b60695b549dc45fc2b7a3d0
Nhinds88 <nhinds@csumb.edu>
2020-09-09 21:13:01 -0700

    reworked people counter

--- people_counter.py
+++ people_counter.py
@@ -4,118 +4,36 @@ Created on Tue Sep  1 11:34:28 2020
 
 @author: NicholasHinds
 """
-
-import numpy
 import cv2
 import tools
 
-try: 
-    log = open('log.txt', 'w')
-except:
-    print('Cannot open log file')
+# =============================================================================
+# try: 
+#     log = open('log.txt', 'w')
+# except:
+#     print('Cannot open log file')
+# =============================================================================
 
 #cameraFeed = tools.cameraSource(0)
 #cameraFeed = tools.cameraSource('TestVideo.avi')
 #cameraFeed = tools.cameraSource('https://r4---sn-a5mlrnez.googlevideo.com/videoplayback?expire=1599358291&ei=8_BTX6rlNIbakgaayoKQDw&ip=76.103.89.157&id=o-AK75r7WK7a2nSCa6muKbbTJAg3hG-4lu0iotr2nRCB4L&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&gir=yes&clen=729683&ratebypass=yes&dur=16.021&lmt=1580854655287187&fvip=4&c=WEB&txp=1311222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRAIgRUSTMoRdCCc0GsSN2FAgpqaHjU7_O6Qso6N2zJ5_yC4CICrlRdeAqppoyf2hKvgRnGCF_Toi8fTKB9nRpkaeigV3&redirect_counter=1&cm2rm=sn-n4vls7z&req_id=745e6a7fca55a3ee&cms_redirect=yes&mh=2R&mm=34&mn=sn-a5mlrnez&ms=ltu&mt=1599336628&mv=m&mvi=4&pl=23&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhAKBhxlOlWeGHGAu0SBJSOtRrbbXCZ3Welkho91P7Kzd0AiEA0DE9S5OprKA1h8CTaVsRYNdYGz3nQyKsRopw09DyqWw%3D')
-cameraFeed = tools.cameraSource('Pexels Videos 4698.mp4')
+#cameraFeed = tools.cameraSource('Pexels Videos 4698.mp4')
+cameraFeed = tools.cameraSource('videoplayback.mp4')
 
 video = cv2.VideoCapture(cameraFeed)
 
-height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-frameArea = height * width
-areaThreshold = frameArea/250
+values = list()
+motion = list()
+avg = None
+count1 = 0
+count2 = 0
+    
+tools.process_video(video, values, motion, count1, count2, avg)
 
-#entry_line = tools.lineCrossingPlacement(int((2*(height/5))), 'y')
-#exit_line = tools.lineCrossingPlacement(int(entry_line[0] + 100), 'y')
-#entry_line_Limit = tools.lineCrossingPlacement(int((1*(height/5))), 'y')
-#exit_line_Limit = tools.lineCrossingPlacement(int((4*(height/5))), 'y')
-
-entry_line = tools.lineCrossingPlacement(int(0), 'x') #Entry Start
-exit_line = tools.lineCrossingPlacement(int(1000), 'x')
-entry_line_Limit = tools.lineCrossingPlacement(int(600), 'x')
-exit_line_Limit = tools.lineCrossingPlacement(int(0), 'x') #Entry End
-                                        
-point1 = [0,0]
-point2 = [0,0]
-point3 = [0,0]
-point4 = [0,0]
-point5 = [0,0]
-point6 = [0,0]
-point7 = [0,0]
-point8 = [0,0]
-    
-if entry_line[1] == 'y':
-    point3 = [0, entry_line[0]]
-    point4 = [width, entry_line[0]]
-    print('Entry line Y')
-else:
-    point3 = [entry_line[0], 0]
-    point4 = [entry_line[0], height]
-    print('Entry line X')
-
-if exit_line[1] == 'y':
-    point1 = [0, exit_line[0]]
-    point2 = [width, exit_line[0]]
-    print('Exit line Y')
-else:
-    point1 = [exit_line[0], 0]
-    point2 = [exit_line[0], height]
-    print('Exit line X')
-    
-if entry_line_Limit[1] == 'y':
-    point5 = [0, entry_line_Limit[0]]
-    point6 = [width, entry_line_Limit[0]]
-    print('Entry line Limit Y')
-else:
-    point5 = [entry_line_Limit[0], 0]
-    point6 = [entry_line_Limit[0], height]
-    print('Entry line Limit X')
-    
-if exit_line_Limit[1] == 'y':
-    point7 = [0, exit_line_Limit[0]]
-    point8 = [width, exit_line_Limit[0]]
-    print('Exit line Limit Y')
-else:
-    point7 = [exit_line_Limit[0], 0]
-    point8 = [exit_line_Limit[0], height]
-    print('Exit line Limit X')
-    
-# =============================================================================x
-line1_points = numpy.array([point1,point2], numpy.int32)
-line1_points = line1_points.reshape((-1,1,2))
 # =============================================================================
-line2_points = numpy.array([point3,point4], numpy.int32)
-line2_points = line2_points.reshape((-1,1,2))
-# =============================================================================
-line3_points = numpy.array([point5,point6], numpy.int32)
-line3_points = line3_points.reshape((-1,1,2))
-# =============================================================================
-line4_points = numpy.array([point7,point8], numpy.int32)
-line4_points = line4_points.reshape((-1,1,2))
+# log.flush()
+# log.close()
 # =============================================================================
 
-persons = []
-
-while video.isOpened():
-    
-    ret, frame = video.read()
-    
-    for i in persons:
-        i.age_one()
-        
-    if ret == True:
-        tools.process_video(log, ret, frame, areaThreshold, 
-                            entry_line_Limit, entry_line, 
-                            exit_line_Limit, exit_line, 
-                            line1_points, line2_points, 
-                            line3_points, line4_points, 
-                            persons)
-    
-    if cv2.waitKey(5) == ord('x'):
-            break
-
-log.flush()
-log.close()
 video.release()
 cv2.destroyAllWindows()
--- tools.py
+++ tools.py
@@ -6,19 +6,7 @@ Created on Tue Sep  1 11:54:34 2020
 """
 
 import cv2
-import time
-import numpy
-import Person
 import imutils
-
-subtractor = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
-
-Op = numpy.ones((3,3),numpy.uint8)
-Cl = numpy.ones((11,11),numpy.uint8)
-
-pid = 0
-entry = 0
-exited = 0
 
 def cameraSource(source):
     return source
@@ -42,116 +30,82 @@ def majority(x):
     
     return max
 
-def process_video(video):
-    ret, frame = video.read()
-    flag = True
-    text = ""
+def process_video(video, values, motion, count1, count2, avg):
     
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    while video.isOpened():
     
-    if avg is None:
-        avg = gray.copy().astype("float")
-        continue
-    
-    cv2.accumulateWeighted(gray, avg, 0.5)
-    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-    thresh = cv2.threshold(frameDelta, 5, 255, cv2. )
-# =============================================================================
-# def process_video(log, ret, frame, areaThreshold, entry_limit, entry_line, exit_limit, exit_line, line1, line2, line3, line4, persons):
-#     font = cv2.FONT_ITALIC
-#     max_p_age = 5
-#     
-#     global pid
-#     global entry
-#     global exited
-#     
-#     mask1 = subtractor.apply(frame)
-#     mask2 = subtractor.apply(frame)
-#     
-#     try:
-#         ret, imbin1 = cv2.threshold(mask1, 200, 255, cv2.THRESH_BINARY)
-#         ret, imbin2 = cv2.threshold(mask2, 200, 255, cv2.THRESH_BINARY)
-#          
-#         #Opening
-#         maskA = cv2.morphologyEx(imbin1, cv2.MORPH_OPEN, Op)
-#         maskB = cv2.morphologyEx(imbin2, cv2.MORPH_OPEN, Op)
-#          
-#         #Closing
-#         maskA = cv2.morphologyEx(maskA, cv2.MORPH_CLOSE, Cl)
-#         maskB = cv2.morphologyEx(maskB, cv2.MORPH_CLOSE, Cl)
-#     except :
-#         print('Enter: ', entry)
-#         print('Exited: ', exited)
-#         
-#     
-#     contours, hierarchy = cv2.findContours(maskB, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     for count in contours:
-#         area = cv2.contourArea(count)
-#         
-#         if area > areaThreshold:
-#             
-#             M = cv2.moments(count)
-#             cx = int(M['m10']/M['m00'])
-#             cy = int(M['m01']/M['m00'])
-#             x,y,w,h = cv2.boundingRect(count)
-#             
-#             new = True
-#             
-#             if cy in range(int(entry_limit[0]), int(exit_limit[0])):
-#                 for i in persons:
-#                     if abs(x - i.getX()) <= w and abs(y - i.getY()) <= h:
-#                         
-#                         new = False
-#                         i.updateCoords(cx, cy)
-#                         if i.going_UP(exit_line[0], entry_line[0]) == True:
-#                             entry += 1
-#                             print('Entered ' + str(entry))
-#                             # print( "ID:",i.getId(),'crossed going up at',time.strftime("%c"))
-#                             log.write("ID: "+str(i.getId())+' crossed going Entry at ' + time.strftime("%c") + '\n')
-#                         elif i.going_DOWN(exit_line[0], entry_line[0]) == True:
-#                             exited += 1
-#                             print('Exited ' + str(exited))
-#                             # print( "ID:",i.getId(),'crossed going down at',time.strftime("%c"))
-#                             log.write("ID: " + str(i.getId()) + ' crossed going Exit at ' + time.strftime("%c") + '\n')
-#                         break
-#                     
-#                     if i.getState() == '1':
-#                         
-#                         if i.getDir() == 'exit' and i.getY() > exit_limit:
-#                             i.setDone()
-#                         elif i.getDir() == 'entry' and i.getY() < entry_limit:
-#                             i.setDone()
-#                     if i.timedOut():
-#                         
-#                         index = persons.index(i)
-#                         persons.pop(index)
-#                         del i
-#                 
-#                 if new == True:
-#                     p = Person.MyPerson(pid, cx, cy, max_p_age)
-#                     persons.append(p)
-#                     pid += 1
-#                     
-#             cv2.circle(frame, (cx, cy), 5, (0,0,255), -1)
-#             
-#     for i in persons:
-#         cv2.putText(frame, str(i.getId()), (i.getX(), i.getY()), font, 0.3, i.getRGB(), 1, cv2.LINE_AA)
-#         
-#     enter_str = 'Entered: ' + str(entry)
-#     exit_str = 'Exited: ' + str(exited)
-#     
-#     frame = cv2.polylines(frame, [line1], False, (255,0,0), thickness=2)
-#     frame = cv2.polylines(frame, [line2], False, (0,0,255), thickness=2)
-#     frame = cv2.polylines(frame, [line3], False, (0,255,0), thickness=1)
-#     frame = cv2.polylines(frame, [line4], False, (255,255,51), thickness=1)
-#     
-#     cv2.putText(frame, enter_str, (10,40), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
-#     cv2.putText(frame, enter_str, (10,40), font, 0.5, (0,0,255), 1, cv2.LINE_AA)
-#     cv2.putText(frame, exit_str, (10,90), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
-#     cv2.putText(frame, exit_str, (10,90), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
-#     
-#     cv2.imshow('Frame', frame)
-#     #cv2.imshow('Mask', maskA)
-# =============================================================================
+        ret, frame = video.read()
+        flag = True
+        # text = ""
+            
+        if ret == True:
+            
+            frame = imutils.resize(frame, width=500)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
+                
+            if avg is None:
+                avg = gray.copy().astype("float")
+                continue
+                
+            cv2.accumulateWeighted(gray, avg, 0.5)
+            frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+            thresh = cv2.threshold(frameDelta, 5, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations = 2)
+            count, height = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                
+            for c in count:
+                    
+                if cv2.contourArea(c) < 5000:
+                    continue
+                    
+                (x, y, w, h) = cv2.boundingRect(c)
+                values.append(x)
+                t1 = x + w
+                t2 = y + h
+                cv2.rectangle(frame, (x, y), (t1, t2), (0, 0, 255), 2)
+                flag = False
+                    
+            noX = len(values)
+                
+            if (noX > 2):
+                    
+                difference = values[noX - 1] - values[noX - 2]
+                    
+                if (difference > 0):
+                        
+                    motion.append(1)
+                        
+                else:
+                        
+                    motion.append(0)
+                        
+            if flag is True:
+                    
+                if (noX > 5):
+                        
+                    val, times = majority(motion)
+                        
+                    if val == 1 and times >= 15:
+                            
+                        count1 += 1
+                            
+                    else:
+                            
+                        count2 += 1
+                            
+                values = list()
+                motion = list()
+                    
+            cv2.line(frame, (260, 0), (260, 480), (0, 255, 0), 2)
+            cv2.line(frame, (420, 0), (420, 480), (255, 0, 0), 2)
+                
+            cv2.putText(frame, "Enter: {}".format(count1), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0 , 255), 2)
+            cv2.putText(frame, "Exit: {}".format(count2), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                
+            cv2.imshow("Frame", frame)
+            cv2.imshow("Gray", gray)
+            cv2.imshow("FrameDelta", frameDelta)
+                
+            if cv2.waitKey(5) == ord('x'):
+                break
