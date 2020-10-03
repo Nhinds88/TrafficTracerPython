c145ffdc8e3a37ee64c600a7193e91cb54dd269a
Nhinds88 <nhinds@csumb.edu>
2020-10-03 10:33:52 -0700

    ip camera information

--- tools.py
+++ tools.py
@@ -8,8 +8,18 @@ Created on Tue Sep  1 11:54:34 2020
 import cv2
 import Person
 import mysql.connector
+import numpy as np
+import os
 
 from datetime import datetime
+
+# Standard Video Dimensions Sizes
+STD_DIMENSIONS =  {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
 
 def cameraSource(source):
     return source
@@ -59,8 +69,36 @@ def insertPeopleData(ete, pid, mid, dur, date, t, db):
     cursor.execute(sql, val)
     
     db.commit()
-
     
+# Set resolution for the video capture
+# Function adapted from https://kirr.co/0l6qmh
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+    
+# grab resolution dimensions and set video capture to it.
+def get_dims(cap, res='1080p'):
+    width, height = STD_DIMENSIONS["480p"]
+    if res in STD_DIMENSIONS:
+        width,height = STD_DIMENSIONS[res]
+    ## change the current caputre device
+    ## to the resulting resolution
+    change_res(cap, width, height)
+    return width, height
+
+# Video Encoding, might require additional installs
+# Types of Codes: http://www.fourcc.org/codecs.php
+VIDEO_TYPE = {
+    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+}
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+      return  VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']    
 
 def process_video(video, lineStart, lineEnd, v_or_h, contourLimit, merchantid, db):
     
--- traffictracer.sql
+++ traffictracer.sql
@@ -35,7 +35,7 @@ CREATE TABLE `foottraffic` (
   PRIMARY KEY (`trafficid`),
   KEY `merchantid_idx` (`merchantid`),
   CONSTRAINT `merchantid` FOREIGN KEY (`merchantid`) REFERENCES `merchant` (`merchantid`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -77,6 +77,10 @@ CREATE TABLE `users` (
 --
 -- Dumping events for database 'traffictracer'
 --
+
+--
+-- Dumping routines for database 'traffictracer'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -87,4 +91,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-17 16:18:50
+-- Dump completed on 2020-09-22 22:19:06
